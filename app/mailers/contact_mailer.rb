9999c23a7ee922321cf7fe3d8a23a9e21f2381f6
good-life-assist <info@good-life-assist.com>
2023-04-05 17:08:10 +0900

    Update contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def send_mail(contact)
     @contact = contact
-    mail from: '"株式会社グッドライフアシスト" <goodlifeassist.murakami@gmail.com>', reply_to: @contact.email, to: '"株式会社グッドライフアシスト" <info@good-life-assist.com>', subject: "お客様からの新しいメッセージ（作成日：#{@contact.created_at.strftime('%Y/%m/%d')}）"
+    mail from: '"株式会社グッドライフアシスト" <goodlifeassist.murakami@gmail.com>', reply_to: @contact.email, to: '"株式会社グッドライフアシスト" <goodlifeassist.murakami@gmail.com>', subject: "お客様からの新しいメッセージ（作成日：#{@contact.created_at.strftime('%Y/%m/%d')}）"
   end
 end
