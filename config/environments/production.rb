be04a378d49e27b786bb3e362569a4cce6e71aa1
good-life-assist <info@good-life-assist.com>
2023-04-05 17:14:32 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,6 +101,6 @@ Rails.application.configure do
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     enable_starttls_auto: true,
-    authentication: :login
+    authentication: :plain
   }
 end
